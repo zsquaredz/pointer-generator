185b1ea3b42f573e8c7d3b20b378bbbfbec8a9b2
zsquaredz <zhaozheng1997@hotmail.com>
2020-01-12 16:49:18 +0000

    run exp

--- scripts/combineOutput.py
+++ scripts/combineOutput.py
@@ -16,9 +16,9 @@ def combine(dir, fileName):
 
 if __name__ == '__main__':
     # combine output
-    combine('/disk/ocean/zheng/pointer-generator/logs/pretrained_model_tf1.2.1/decode_test_400maxenc_4beam_35mindec_100maxdec_ckpt-238410/decoded/',
+    combine('/disk/ocean/zheng/pointer-generator/logs/pretrained_model_tf1.2.1/decode_test_400maxenc_4beam_35mindec_120maxdec_ckpt-238410/decoded/',
             'decoded.txt')
 
     # combine reference
-    combine('/disk/ocean/zheng/pointer-generator/logs/pretrained_model_tf1.2.1/decode_test_400maxenc_4beam_35mindec_100maxdec_ckpt-238410/reference/',
+    combine('/disk/ocean/zheng/pointer-generator/logs/pretrained_model_tf1.2.1/decode_test_400maxenc_4beam_35mindec_120maxdec_ckpt-238410/reference/',
             'reference.txt')
